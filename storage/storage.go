b871fbbc4bd83750a9e7945c1428ab6b1dc79725
Ilham Syahid S <ilhamsyahids@gmail.com>
2023-09-12 08:32:55 +0700

    feat: update storage todo
    Signed-off-by: Ilham Syahid S <ilhamsyahids@gmail.com>

--- storage/storage.go
+++ storage/storage.go
@@ -1,10 +1,9 @@
 package storage
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"strings"
+
+	"github.com/jmoiron/sqlx"
 )
 
 var BookNotFound = fmt.Errorf("book not found")
@@ -18,93 +17,54 @@ type Book struct {
 }
 
 type Storage struct {
-	booksData []Book
+	sqlClient *sqlx.DB
 }
 
-func NewStorage() *Storage {
-	return &Storage{}
-}
-
-func (s *Storage) Load(filename string) error {
-	rawBook, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return fmt.Errorf("unable to read file due: %v", err)
-	}
-
-	err = json.Unmarshal(rawBook, &s.booksData)
-	if err != nil {
-		return fmt.Errorf("unable to init books data due: %v", err)
-	}
-
-	return nil
+func NewStorage(sqlClient *sqlx.DB) *Storage {
+	return &Storage{sqlClient: sqlClient}
 }
 
 func (s *Storage) GetBooks(query string, page, limit int) ([]Book, error) {
-	var books []Book
-	for _, book := range s.booksData {
-		lowerTitle := strings.ToLower(book.Title)
-		lowerQuery := strings.ToLower(query)
-		if strings.Contains(lowerTitle, lowerQuery) {
-			books = append(books, book)
-		}
+	// get books from database
+	books := []Book{}
+	sqlQuery := `SELECT * FROM books WHERE title LIKE ? LIMIT ? OFFSET ?`
+	err := s.sqlClient.Select(&books, sqlQuery, "%"+query+"%", limit, (page - 1))
+	if err != nil {
+		return nil, err
 	}
-
-	// pagination
-	startIndex := (page - 1) * limit
-	endIndex := page * limit
-	if startIndex >= len(books) {
-		return []Book{}, nil
-	}
-	if endIndex > len(books) {
-		endIndex = len(books)
-	}
-	books = books[(page-1)*limit : endIndex]
-
 	return books, nil
 }
 
 func (s *Storage) CreateBook(book Book) (*Book, error) {
-	book.ID = fmt.Sprintf("%d", len(s.booksData)+1)
-	s.booksData = append(s.booksData, book)
+	// insert book to database
+	sqlQuery := `INSERT INTO books (isbn, title, author, published) VALUES (?, ?, ?, ?)`
+	res, err := s.sqlClient.Exec(sqlQuery, book.ISBN, book.Title, book.Author, book.Published)
+	if err != nil {
+		return nil, err
+	}
+
+	// get last inserted id
+	id, err := res.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+	// set id to book
+	book.ID = fmt.Sprintf("%d", id)
+
 	return &book, nil
 }
 
 func (s *Storage) GetBookByID(id string) (*Book, error) {
-	for _, book := range s.booksData {
-		if book.ID == id {
-			return &book, nil
-		}
-	}
-	return nil, BookNotFound
+	// TODO: implement this
+	return nil, nil
 }
 
 func (s *Storage) UpdateBook(id string, book Book) (*Book, error) {
-	for i, b := range s.booksData {
-		if b.ID == id {
-			if book.ISBN != "" {
-				s.booksData[i].ISBN = book.ISBN
-			}
-			if book.Title != "" {
-				s.booksData[i].Title = book.Title
-			}
-			if book.Author != "" {
-				s.booksData[i].Author = book.Author
-			}
-			if book.Published != "" {
-				s.booksData[i].Published = book.Published
-			}
-			return &s.booksData[i], nil
-		}
-	}
-	return nil, BookNotFound
+	// TODO: implement this
+	return nil, nil
 }
 
 func (s *Storage) DeleteBook(id string) error {
-	for i, book := range s.booksData {
-		if book.ID == id {
-			s.booksData = append(s.booksData[:i], s.booksData[i+1:]...)
-			return nil
-		}
-	}
-	return BookNotFound
+	// TODO: implement this
+	return nil
 }
